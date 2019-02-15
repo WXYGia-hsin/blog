b18a294d9d8b873619e10b8c18c7ea60c9e85505
WXYGia-hsin <2223775729@qq.com>
2019-02-15 17:30:59 +0800

    评论管理

--- src/main/java/com/wxy/blog/controller/CommentController.java
+++ src/main/java/com/wxy/blog/controller/CommentController.java
@@ -1,13 +1,22 @@
 package com.wxy.blog.controller;
 
+import com.wxy.blog.entity.Blog;
+import com.wxy.blog.entity.Comment;
+import com.wxy.blog.entity.User;
 import com.wxy.blog.service.BlogService;
 import com.wxy.blog.service.CommentService;
+import com.wxy.blog.util.ConstraintViolationExceptionHandler;
+import com.wxy.blog.vo.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
 
 @Controller
 @RequestMapping("/comments")
@@ -17,10 +26,81 @@ public class CommentController {
 
     @Autowired
     private CommentService commentService;
+
+    /**
+     * 获取评论列表
+     * @param blogId
+     * @param model
+     * @return
+     */
     @GetMapping
     public String listComment(@RequestParam(value="blogId",required=true) Long blogId, Model model){
+        Blog blog=blogService.getBlogById(blogId);
 
-        return "";
+        List<Comment> comments = blog.getComments();
+
+        // 判断操作用户是否是评论的所有者
+        String commentOwner = "";
+        if (SecurityContextHolder.getContext().getAuthentication() !=null && SecurityContextHolder.getContext().getAuthentication().isAuthenticated()
+                &&  !SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString().equals("anonymousUser")) {
+            User principal = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            if (principal !=null) {
+                commentOwner = principal.getUsername();
+            }
+        }
+        model.addAttribute("commentOwner", commentOwner);
+        model.addAttribute("comments", comments);
+        return "/userspace/blog :: #mainContainerRepleace";
     }
+
+    /**
+     * 发表评论
+     * @param blogid
+     * @param commentContent
+     * @return
+     */
+    @PostMapping
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_USER')")
+    public ResponseEntity<Response> createCommment(Long blogid,String commentContent){
+        try {
+            blogService.createComment(blogid,commentContent);
+        }catch (ConstraintViolationException e) {
+            return  ResponseEntity.ok(new Response(false, ConstraintViolationExceptionHandler.getMessage(e)));
+        }
+        catch (Exception e) {
+            return  ResponseEntity.ok(new Response(false,e.getMessage()));//返回该异常描述的字符串
+        }
+        return  ResponseEntity.ok(new Response(true,"处理成功","null"));
+    }
+
+    @DeleteMapping("{/id}")
+    public ResponseEntity<Response> delectBlog(@PathVariable("id") Long id,Long blogId){
+        boolean isOwner=false;
+        User user=commentService.getCommentById(id).getUser();
+        // 判断操作用户是否是博客的所有者
+        if (SecurityContextHolder.getContext().getAuthentication() !=null && SecurityContextHolder.getContext().getAuthentication().isAuthenticated()
+                &&  !SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString().equals("anonymousUser")) {
+            User principal = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            if (principal !=null && user.getUsername().equals(principal.getUsername())) {
+                isOwner = true;
+            }
+        }
+
+        if (!isOwner) {
+            return ResponseEntity.ok().body(new Response(false, "没有操作权限"));
+        }
+
+        try {
+            blogService.removeComment(blogId, id);
+            commentService.removeComment(id);
+        } catch (ConstraintViolationException e)  {
+            return ResponseEntity.ok().body(new Response(false, ConstraintViolationExceptionHandler.getMessage(e)));
+        } catch (Exception e) {
+            return ResponseEntity.ok().body(new Response(false, e.getMessage()));
+        }
+        return  ResponseEntity.ok(new Response(true,"处理成功","null"));
+    }
+
+
 
 }
